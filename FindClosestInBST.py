c24b741ac35e85104b0ec66e53503b00d67433e7
jssanju2999 <jssanju@live.com>
2020-04-19 22:07:15 +0530

    Find_Closest

---
+++ FindClosestInBST.py
@@ -0,0 +1,14 @@
+def findClosestValueInBst(tree, target):
+    return findClosestValueInBstHelper(tree,target,float("inf"))
+	
+def findClosestValueInBstHelper(tree, target,closest):
+	if tree is None:
+		return closest
+	if abs(closest-target) > abs(target-tree.value):
+		closest=tree.value
+	if target<tree.value:
+		return findClosestValueInBstHelper(tree.left, target, closest)
+	elif target>tree.value:
+		return findClosestValueInBstHelper(tree.right, target, closest)
+	else:
+		return closest
